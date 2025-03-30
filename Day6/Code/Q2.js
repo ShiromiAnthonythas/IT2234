7d3d3a6e3691011c999406228bace570dda72ec3
ShiromiAnthonythas <shiromii2002@gmail.com>
2025-03-30 23:22:41 +0530

    Add files via upload

---
+++ Day6/Code/Q2.js
@@ -0,0 +1,17 @@
+
+let students = [
+    { regno: "2021/ICT/01", name: "James", age: 21, course: "IT", Pro_languages: ["Java", "JS", "C++"] },
+    { regno: "2021/ICT/02", name: "Sophia", age: 22, course: "CS", Pro_languages: ["Python", "C", "Go"] },
+    { regno: "2021/ICT/03", name: "Michael", age: 23, course: "Software Engineering", Pro_languages: ["C#", "JavaScript", "Swift"] },
+    { regno: "2021/ICT/04", name: "Emma", age: 21, course: "Data Science", Pro_languages: ["Python", "R", "SQL"] },
+    { regno: "2021/ICT/05", name: "Daniel", age: 22, course: "Cybersecurity", Pro_languages: ["Ruby", "Bash", "C"] }
+];
+
+let uniqueLanguages = new Set();
+
+students.forEach(student => {
+    student.Pro_languages.forEach(language => uniqueLanguages.add(language));
+});
+
+let result = Array.from(uniqueLanguages);
+console.log(result);
---
+++ Day6/Code/Q3.js
@@ -0,0 +1,15 @@
+function getMaxValue(num) {
+    if (num < 10) {
+        throw new Error("Number should have a minimum of 2 digits.");
+    }
+
+    return parseInt(num.toString().split('').sort((a, b) => b - a).join(''), 10);
+}
+
+
+const numbers = [215, 1093];
+
+numbers.forEach(num => {
+    console.log(`Input number = ${num}, Max = ${getMaxValue(num)}`);
+});
+
